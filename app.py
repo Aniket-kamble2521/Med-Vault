dee38f09f53072ec7409eb07a417f1032b849314
ANIKET KAMBLE <aniketkamble@ANIKETs-MacBook-Air.local>
2026-04-03 09:13:59 +0530

    fixed port for railway

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ from app_pkg import create_app
 if __name__ == "__main__":
     app = create_app()
     # Allow connections from any device on the network
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
